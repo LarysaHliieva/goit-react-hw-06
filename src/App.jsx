4d8b3e41df487695f6badf353b43227eaf06c91c
LarysaHliieva <lora.glievaya@gmail.com>
2025-08-04 20:40:00 +0300

    add persist

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,6 @@ import styles from "./App.module.css";
 import "./App.css";
 
 function App() {
-  // const [contacts, setContacts] = useState(() => {
-  //   return JSON.parse(localStorage.getItem("contacts")) ?? [];
-  // });
-
-  // useEffect(() => {
-  //   localStorage.setItem("contacts", JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <div className={styles.phonebook}>
       <h1>Phonebook</h1>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import contactsReducer from "./contactSlice";
 import filtersReducer from "./filtersSlice";
 
+const persistContactsReducer = persistReducer(
+  {
+    key: "root",
+    storage,
+    whitelist: ["items"],
+  },
+  contactsReducer
+);
+
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistContactsReducer,
     filters: filtersReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
